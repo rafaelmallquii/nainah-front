fd68a8846f5781e1448a6ffba4fddd5df1092ce0
Yoto <yoto@dev.com>
2023-06-06 14:08:16 -0500

    payment/3

--- src/app/payment/page.js
+++ src/app/payment/page.js
@@ -11,64 +11,11 @@ const authData = {
 };
 
 
-
-const BasicCardInfo = {
-  cardNumber: 4111111111111111,
-  cardCode: 123,
-  month: 12,
-  year: 25,
-};
-
 const App = () => {
-  const { dispatchData, loading, error } = useAcceptJs({ authData });
-  const [cardData, setCardData] = React.useState(BasicCardInfo);
-
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    // Dispatch CC data to Authorize.net and receive payment nonce for use on your server
-    const response = await dispatchData({ cardData });
+  const handleSubmit = (response) => {
     console.log('Received response:', response);
   };
-
-  return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        name="cardNumber"
-        value={cardData.cardNumber}
-        onChange={(event) =>
-          setCardData({ ...cardData, cardNumber: event.target.value })
-        }
-      />
-      <input
-        type="text"
-        name="month"
-        value={cardData.month}
-        onChange={(event) =>
-          setCardData({ ...cardData, month: event.target.value })
-        }
-      />
-      <input
-        type="text"
-        name="year"
-        value={cardData.year}
-        onChange={(event) =>
-          setCardData({ ...cardData, year: event.target.value })
-        }
-      />
-      <input
-        type="text"
-        name="cardCode"
-        value={cardData.cardCode}
-        onChange={(event) =>
-          setCardData({ ...cardData, cardCode: event.target.value })
-        }
-      />
-      <button type="submit" disabled={loading || error}>
-        Pay
-      </button>
-    </form>
-  );
+  return <HostedForm authData={authData} onSubmit={handleSubmit} />;
 };
 
 export default App;
