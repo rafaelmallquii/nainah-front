1239d65c6688ccc19afe8a215861642ac43a635e
Joseph <jvcallupe69@gmail.com>
2023-06-05 20:03:56 -0500

    Delete Test

--- src/app/about/page.js
+++
@@ -1,9 +0,0 @@
-export const metadata = {
-  title: "About Us",
-  description:
-    "This is a meta description. Welcome to slingacademy.com. Happy coding and have a nice day",
-};
-
-export default function () {
-  return <div>page</div>;
-}
