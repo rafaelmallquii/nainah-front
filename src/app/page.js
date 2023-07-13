f2d75c6ecc5d9261803d8c5b81e4eabe10b0ede7
Patrick <74751873+PatrickCuentas@users.noreply.github.com>
2023-07-13 16:00:58 -0500

    fix/rebase delete metadata

--- src/app/page.js
+++ src/app/page.js
@@ -8,11 +8,11 @@ import NewCollectionSection from "@/app/components/NewCollectionSection";
 import HandbagsSection from "@/app/components/HandbagsSection";
 import Main from "@/lib/ui/Main";
 
-export const metadata = {
-  title: "Home",
-  description:
-    "This is a meta description. Welcome to slingacademy.com. Happy coding and have a nice day"
-};
+// export const metadata = {
+//   title: "Home",
+//   description:
+//     "This is a meta description. Welcome to slingacademy.com. Happy coding and have a nice day"
+// };
 
 export default function () {
   return (
