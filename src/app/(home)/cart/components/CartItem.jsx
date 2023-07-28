d7218773a021949d243b16aaffd1fb01869b84b4
Patrick <74751873+PatrickCuentas@users.noreply.github.com>
2023-07-27 19:07:17 -0500

    fix/rebase cartItem null properties fixed

--- src/app/(home)/cart/components/CartItem.jsx
+++ src/app/(home)/cart/components/CartItem.jsx
@@ -10,7 +10,7 @@ export default function CartItem({ product }) {
   return (
     <li className="mr-3 flex items-center justify-between relative">
       <button
-        onClick={() => removeItemFromCart(product.variant.title)}
+        onClick={() => removeItemFromCart(product?.variant?.title)}
         className="top-0 right-4 absolute"
       >
         <img src="/icons/delete.svg" alt="Delete Icon" />
@@ -18,7 +18,7 @@ export default function CartItem({ product }) {
       <div className="flex gap-[15px]">
         <div>
           <Image
-            src={product.variant.image}
+            src={product?.variant?.image}
             width={120}
             height={120}
             className="max-w-full object-cover object-center"
@@ -31,20 +31,20 @@ export default function CartItem({ product }) {
             <span className="font-semibold text-base truncate lg:max-w-[25ch]">
               {product.variant.title}
             </span>
-            <span className="text-sm">{product.title}</span>
+            <span className="text-sm">{product?.title}</span>
             <span className="font-semibold text-base">
-              US$ {product.totalPrice}
+              US$ {product?.totalPrice}
             </span>
             <div className="inline-flex items-center gap-2">
               <span className="text-sm">Color:</span>
               <div
                 className="w-3 h-3 rounded-full"
                 style={{
-                  backgroundColor: colors[product.variant.color] ?? "gray"
+                  backgroundColor: colors[product?.variant?.color] ?? "gray"
                 }}
               ></div>
             </div>
-            <span className="text-sm">Size: {sizes[product.variant.size]}</span>
+            <span className="text-sm">Size: {sizes[product?.variant?.size]}</span>
           </div>
         </section>
 
